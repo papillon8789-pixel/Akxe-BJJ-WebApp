d1fb26196d30fcdf7bd50ade174aa9f53b2187be
papillon8789-pixel <papillon8789@gmail.com>
2026-09-09 10:49:56 +0200

    Add professional BJJ category icons with text labels

--- src/components/layout/IconNavigation.jsx
+++ src/components/layout/IconNavigation.jsx
@@ -17,8 +17,15 @@ export default function IconNavigation() {
                 : 'bg-gray-800 text-gray-400 hover:bg-gray-700 hover:text-white'
             }`}
           >
-            <span className="text-2xl">{cat.icon}</span>
-            <span className="text-xs font-semibold whitespace-nowrap">{cat.label}</span>
+            {cat.iconType === 'image' ? (
+              <img
+                src={cat.icon}
+                alt={cat.label}
+                className="w-16 h-16 object-contain"
+              />
+            ) : (
+              <span className="text-2xl">{cat.icon}</span>
+            )}
           </button>
         ))}
       </div>
